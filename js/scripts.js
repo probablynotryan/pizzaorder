eac82e2a6dd5982b78c952fa4e3f2a62a0738e20
Ryan Bass <theryanbass@gmail.com>
2022-01-28 08:30:21 -0800

    add Pizza constructor

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// Business Logic
+function Pizza(size, pepperoni, cheese) {
+  this.size = size;
+  this.pepperoni = pepperoni;
+  this.cheese = cheese;
+}
+
+pizza1 = new Pizza("Medium", "Pepperoni", "No Cheese");
+
+
+
+
+
+
+// User Interface
