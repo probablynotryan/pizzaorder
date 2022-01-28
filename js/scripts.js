992882aac0a4d22770ca1e97a589df7493496e41
Ryan Bass <theryanbass@gmail.com>
2022-01-28 09:05:56 -0800

    add initialization of PizzaOrder object

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,23 @@ function Pizza(size, pepperoni, cheese) {
   this.cheese = cheese;
 }
 
-pizza1 = new Pizza("Medium", "Pepperoni", "No Cheese");
+function PizzaOrder() {
+  this.pizzas = {}
+  this.currentPizza = 0;
+}
 
+PizzaOrder.prototype.addPizza = function (pizza) {
+  pizza.id = this.assignID();
+  this.pizzas[pizza.id] = pizza;
+}
+
+PizzaOrder.prototype.assignID = function () {
+  this.currentPizza += 1;
+  return this.currentPizza;
+}
+
+let pizza1 = new Pizza("Medium", "Pepperoni", "No Cheese");
+let myPizzaOrder = new PizzaOrder(pizza1);
 
 
 
